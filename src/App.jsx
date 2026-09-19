c92681a1d417807bf2ea9b3a218baf902b1808f9
rashmiw333 <rashmiwankhade99@gmail.com>
2026-09-19 16:13:47 +0530

    resume addition

--- src/App.jsx
+++ src/App.jsx
@@ -102,7 +102,7 @@ function App() {
             </a>
 
             <a
-              href="/Rashmi_Wankhade_Resume_New.pdf"
+              href="/Rashmi Wankhade_Resume_New.pdf"
               className="button secondary-button"
               target="_blank"
               rel="noreferrer"
